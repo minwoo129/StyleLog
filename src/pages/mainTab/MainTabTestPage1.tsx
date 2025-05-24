4052c00f7fe8ceb71076aeb608e0c701a1a4e1a3
minwoo129 <key0129mw@naver.com>
2025-05-24 20:46:55 +0900

    feat: #1 주석 삭제

--- src/pages/mainTab/MainTabTestPage1.tsx
+++ src/pages/mainTab/MainTabTestPage1.tsx
@@ -19,30 +19,12 @@ const MainTabTestPage1: FC<MainTabTestPage1Props> = ({}) => {
 
   const getData = async () => {
     try {
-      // const firebaseapp = await initializeApp({
-      //   apiKey: 'AIzaSyCmBQSNXLNNOjSLIrpV9EX3YPjjg0Wt9SI',
-      //   authDomain: 'stylelogapp-1b1c3.firebaseapp.com',
-      //   projectId: 'stylelogapp-1b1c3',
-      //   storageBucket: 'stylelogapp-1b1c3.firebasestorage.app',
-      //   messagingSenderId: '748700978217',
-      //   appId: '1:748700978217:web:366114d3b33b7e16902038',
-      //   measurementId: 'G-VHEYZCWM7J',
-      //   databaseURL: 'https://stylelogapp-1b1c3-default-rtdb.firebaseio.com',
-      // });
-      // await initializeFirestore(firebaseapp, {
-      //   experimentalAutoDetectLongPolling: true,
-      //   useFetchStreams: false,
-      // });
       const db = getFirestore();
       const testRef = collection(db, 'test');
       const q = query(testRef);
 
       const snapshot = await getDocs(q);
       console.log('snapshot: ', snapshot);
-      // const snapshot = await firestore()
-      //   .collection('test')
-      //   .doc('Skr0SpjzuyUQD4Y4xTgX');
-      // console.log('snapshot: ', snapshot);
     } catch (error) {
       console.log('Error getting documents: ', error);
     }
