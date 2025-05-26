10da31e1dbd3ae4b69025012a725795f680fa0dd
minwoo129 <key0129mw@naver.com>
2025-05-26 13:38:18 +0900

    feat: #1 Firebase Auth 기능 임시 저장

--- src/pages/authStack/Login.tsx
+++ src/pages/authStack/Login.tsx
@@ -2,7 +2,9 @@ import LoginBtn from '@components/pages/authStack/login/LoginBtn';
 import LoginInput from '@components/pages/authStack/login/LoginInput';
 import useLoginInput from '@hooks/pages/authStack/login/useLoginInput';
 import React, {FC} from 'react';
+import {signInWithEmailAndPassword, getAuth} from '@react-native-firebase/auth';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {ReactNativeFirebase} from '@react-native-firebase/app';
 
 interface LoginProps {}
 
@@ -10,9 +12,20 @@ const Login: FC<LoginProps> = ({}) => {
   const [email, onChangeEmail] = useLoginInput();
   const [password, onChangePassword] = useLoginInput();
 
-  const onPressLogin = () => {
+  const onPressLogin = async () => {
     console.log('email:', email);
     console.log('password:', password);
+    try {
+      const res = await signInWithEmailAndPassword(getAuth(), email, password);
+      console.log('Login successful:', res.user);
+    } catch (e) {
+      const error = e as ReactNativeFirebase.NativeFirebaseError;
+
+      if (error.code === 'auth/invalid-credential') {
+        //console.log('Invalid credentials:', error.message);
+        // 회원정보가 존재하지 않는 경우 => 회원가입 페이지로 이동(소셜로그인만 있기 때문)
+      }
+    }
   };
 
   return (
