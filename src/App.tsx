c144d0302ebe13b9101dae08ca660720404ffa9f
minwoo129 <key0129mw@naver.com>
2025-05-28 19:18:38 +0900

    feat: #9 데이터 연결을 위한 기본 UI 컴포넌트 개발

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,24 @@
 import ContextProvider from '@contexts';
 import {AppStateContextProvider} from '@contexts/common/AppStateContext';
 import AppMain from '@pages/root/AppMain';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {Provider} from 'jotai';
 import React from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import * as eva from '@eva-design/eva';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
 function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaProvider>
         <Provider>
+          <IconRegistry icons={EvaIconsPack} />
           <ContextProvider contexts={[AppStateContextProvider]}>
-            <AppMain />
+            <ApplicationProvider {...eva} theme={eva.light}>
+              <AppMain />
+            </ApplicationProvider>
           </ContextProvider>
         </Provider>
       </SafeAreaProvider>
