99c01bbc44e0be7db50edcab6362843fca317680
minwoo129 <key0129mw@naver.com>
2025-05-30 15:42:46 +0900

    feat: #10 상세 페이지 헤더 연결

--- src/hooks/pages/mainStack/LogDetail/useLogDetail.tsx
+++ src/hooks/pages/mainStack/LogDetail/useLogDetail.tsx
@@ -1,15 +1,22 @@
-import {useState} from 'react';
+import {useAppNavigation} from '@hooks/common/navigation';
+import {useCallback, useState} from 'react';
 
 const useLogDetail = () => {
+  const mainStackNavigation = useAppNavigation('MainStack');
   const [selectedIdx, setSelectedIdx] = useState(0);
 
   const onChangeIndex = (idx: number) => {
     setSelectedIdx(idx);
   };
 
+  const onPressBack = useCallback(() => {
+    mainStackNavigation.goBack();
+  }, []);
+
   return {
     selectedIdx,
     onChangeIndex,
+    onPressBack,
   };
 };
 
--- src/pages/mainStack/LogDetail.tsx
+++ src/pages/mainStack/LogDetail.tsx
@@ -1,21 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import CommonHeader, {BackBtn} from '@components/common/CommonHeader';
 import ImageView from '@components/pages/mainStack/logDetail/ImageView';
-import {useAppNavigation, useAppRoute} from '@hooks/common/navigation';
+import {useAppRoute} from '@hooks/common/navigation';
 import useLogDetail from '@hooks/pages/mainStack/LogDetail/useLogDetail';
 import {ViewPager} from '@ui-kitten/components';
-import React, {useState} from 'react';
+import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
 const LogDetail = () => {
-  const mainStackNavigation = useAppNavigation('MainStack');
   const {
     params: {logData},
   } = useAppRoute('MainStack', 'LogDetail');
 
-  const {selectedIdx, onChangeIndex} = useLogDetail();
+  const {selectedIdx, onChangeIndex, onPressBack} = useLogDetail();
 
   return (
     <SafeAreaView style={styles.container}>
+      <CommonHeader left={<BackBtn onPress={onPressBack} />} />
       <ViewPager
         selectedIndex={selectedIdx}
         shouldLoadComponent={index => index === selectedIdx}
